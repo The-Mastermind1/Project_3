a1e99fbed5ff6a7d5b77ca1f8f800c7319b0aa66
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-11-07 00:06:28 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -36,8 +36,8 @@ int main() {
 		x0 = x1;
 	} while (std::fabs(fungf(x1))>tol);
 	cout << "root found:" << x1 << '\n';
-	iterations = 0;
-	x0 = 1.0;
+	cout<<"enter your initial guess again"<<'\n';
+	std::cin>>x0;
 	iterations = 0;
 	cout << "metodos stateroy shmeioy\n";
 	do {
